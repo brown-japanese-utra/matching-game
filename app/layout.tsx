f5ebce03fb5253b039d2d3b33dfcd2423da2b802
DarkShinyGiratina <ross_williams@brown.edu>
2024-09-19 15:57:30 -0400

    hello world worky

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,22 @@ import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../theme";
 
 export const metadata = {
-  title: "Mantine Next.js template",
-  description: "I am using Mantine with Next.js!",
+  title: "日本語のマッチング",
+  description: "Game for matching verbs and images to learn Japanese",
 };
 
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
-        <ColorSchemeScript />
+        <ColorSchemeScript defaultColorScheme="auto" />
         <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme} defaultColorScheme="auto">
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { Center, Grid, Text, Title } from "@mantine/core";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <>
+      <Center>
+        <Title order={1}>日本語のマッチングゲーム</Title>
+      </Center>
+      <Grid>
+        <Grid.Col span={6}>
+          <Center>
+            <Title order={1}>動詞</Title>
+          </Center>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Center>
+            <Title order={1}>写真</Title>
+          </Center>
+        </Grid.Col>
+
+        <Grid.Col span={{ base: 6, sm: 3 }}>teehee</Grid.Col>
+        <Grid.Col span={{ base: 6, sm: 3 }}>teehee</Grid.Col>
+        <Grid.Col span={{ base: 6, sm: 3 }}>teehee</Grid.Col>
+        <Grid.Col span={{ base: 6, sm: 3 }}>teehee</Grid.Col>
+      </Grid>
+    </>
+  );
 }
