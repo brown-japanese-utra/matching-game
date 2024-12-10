3cfe37950891098ffcfe2822374f15247b248844
DarkShinyGiratina <ross_williams@brown.edu>
2024-12-10 14:45:43 -0500

    defauilt dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,12 @@ export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
-        <ColorSchemeScript defaultColorScheme="auto" />
+        <ColorSchemeScript defaultColorScheme="dark" />
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
       </head>
       <body>
-        <MantineProvider theme={theme} defaultColorScheme="auto">
+        <MantineProvider theme={theme} defaultColorScheme="dark">
           {children}
         </MantineProvider>
       </body>
